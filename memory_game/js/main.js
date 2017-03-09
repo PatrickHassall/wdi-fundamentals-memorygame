3ca35ab7c68451582c99b342744db50a306cfdb1
Patrick Hassall <patrick.j.hassall@gmail.com>
2017-03-09 18:17:58 +0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,14 @@
-var cardOne = "King";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "Queen";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = [cards[1]];
+cardsInPlay.push("cardOne");
+console.log("User flipped " + cardOne);
+var cardTwo = [cards[2]];
+cardsInPlay.push("cardTwo");
+console.log("User flipped " + cardTwo);
 
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("Sorry, try again.");
+}
